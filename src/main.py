2fa0a036a3213029d451394ae5002a19524422cb
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2022-06-04 16:24:08 +0200

    Inizializzazione repository

---
+++ src/main.py
@@ -0,0 +1,89 @@
+import subprocess
+import os
+import json
+from json import JSONDecodeError
+
+from adbutils import adb
+
+import click
+
+#   Trovare un modo per generalizzare i comandi da passare alla shell adb
+#   possibile shell interattiva?
+
+#   Realizzare una tabella dove mostra i dati dei dispositivi connessi,
+#   cliccandoci sopra apre scrcpy
+
+#   Quando il programma viene stoppato con command-C/ctrl-C allora deve
+#   effettuare il dump dell'output su file se indicato dalla flag --dump
+
+#   Se specificato dalla flag, al termine del programma disconnetti tutti i dispositivi --kill
+
+devices = []
+
+
+def masscan(scan, port):
+    if (scan is None) and (port is None):
+        return
+
+    if (scan is None) and port:
+        print(f'ADB-GRABBER ERROR: -s/--scan ADDRESS/PORT is required if -p/--port {port} is specified')
+        exit(2)
+    if scan and (port is None):
+        print(f'ADB-GRABBER ERROR: -s/--scan {scan} requires -p/--port PORT')
+        exit(2)
+
+    # Masscan sulla rete.
+    subprocess.run(
+        ['masscan', scan, '-p', port, '-oJ', 'devices.json'],
+        capture_output=False
+    )
+
+
+def load(file):
+    default_file = 'devices.json'
+
+    if file is None:
+        file = default_file
+
+    if not os.path.exists('./' + file):
+        print(f'ADB-GRABBER ERROR: ./{file} does not exists.')
+        exit(2)
+
+    # Caricamento del dump in memoria.
+    with open(file, 'r') as f:
+        try:
+            data = json.load(f)
+        except JSONDecodeError:
+            print(f'ADB-GRABBER ERROR: {file} contains no devices.')
+            exit(2)
+
+        for d in data:
+            devices.append(f"{d['ip']}:{d['ports'][0]['port']}")
+
+    print(devices)
+
+
+def dump(dump):
+    pass
+
+
+def connect():
+    # Connette i dispositivi con adb
+    for addr in devices:
+        adb.connect(addr, timeout=2.0)
+
+    print(adb.device_list())
+
+
+@click.command()
+@click.option('-s', '--scan', help='Rete + maschera per masscan')
+@click.option('-p', '--port', help='Porta/e da passare a masscan')
+@click.option('-f', '--file', help='File da dove caricare gli ip')
+def main(scan, port, file):
+    masscan(scan, port)
+    load(file)
+    connect()
+
+
+if __name__ == '__main__':
+    main()
